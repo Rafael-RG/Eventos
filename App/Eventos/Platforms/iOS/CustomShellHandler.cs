3144b70641ca124c49f4df598d25edb6ee212500
Rafa <Rafa@192.168.1.11>
2024-09-20 23:42:26 -0300

    Fixes UI IOS

--- App/Eventos/Platforms/iOS/CustomShellHandler.cs
+++ App/Eventos/Platforms/iOS/CustomShellHandler.cs
@@ -32,11 +32,26 @@ namespace Eventos.Platforms.iOS
 
             public void UpdateLayout(UITabBarController controller)
             {
+                controller.TabBar.TintColor = UIColorExtensions.FromHex("#D2E898");
+                controller.TabBar.UnselectedItemTintColor = UIColorExtensions.FromHex("#FFFFFF");
+
                 foreach (var tabbarItem in controller.TabBar.Items)
                 {
                     tabbarItem.Image = tabbarItem.Image?.Scale(new CGSize(24, 24));
                 }
             }
+
+            public static class UIColorExtensions
+            {
+                public static UIColor FromHex(string hex)
+                {
+                    var r = Convert.ToByte(hex.Substring(1, 2), 16);
+                    var g = Convert.ToByte(hex.Substring(3, 2), 16);
+                    var b = Convert.ToByte(hex.Substring(5, 2), 16);
+                    return UIColor.FromRGB(r, g, b);
+                }
+            }
+
         }
     }
 }
